6a504bb08fac9c04235f91bef76b3e8be821bb85
Elina Singh <elinasingh1997@gmail.com>
2024-01-09 07:51:47 +0530

    rendering title, content using map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(noteItem) {
+  return (
+    <Note 
+      key= {noteItem.key} 
+      title= {noteItem.title} 
+      content={noteItem.content} 
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note 
-        title= "This is the note title"
-        content= "This is the note content"
-      />
+      {notes.map(createNotes)}
       <Footer />
       
     </div>
