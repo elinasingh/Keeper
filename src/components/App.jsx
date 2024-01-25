70fa2ae0a1dc1224002d5eca08cdf1ecc617065d
Elina Singh <elinasingh1997@gmail.com>
2024-01-25 08:37:22 +0530

    created new component createArea and imported it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
-function createNotes(noteItem) {
-  return (
-    <Note 
-      key= {noteItem.key} 
-      title= {noteItem.title} 
-      content={noteItem.content} 
-    />
-  );
-}
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(createNotes)}
+      {notes.map(noteItem => (
+      <Note 
+        key= {noteItem.key} 
+        title= {noteItem.title} 
+        content={noteItem.content} 
+      />
+))}
+
       <Footer />
       
     </div>
