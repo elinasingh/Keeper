1c48e17d51a87e864180f53de6b42e83200d10f4
Elina Singh <elinasingh1997@gmail.com>
2024-01-25 09:00:06 +0530

    Deleting notes.jsx for dynamic, adding form for notes and adding delete button on notes

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -5,7 +5,7 @@ function CreateArea() {
         <div>
             <form>
                 <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a notee..." rows="3" />
+                <textarea name="content" placeholder="Take a note..." rows="3" />
                 <button>Add</button>
             </form>
         </div>
