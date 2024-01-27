593b467d16ea90b8e9b4ce70361da1520cdee558
Elina Singh <elinasingh1997@gmail.com>
2024-01-27 19:21:35 +0530

    Implementing the delete note functionality and Callback from the Note component to trigger a delete function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,12 +15,22 @@ function App() {
     });
    }
 
+   function deleteNote(id) {
+    console.log("Delete was triggered");
+   }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
       {notes.map((noteItem) => {
-        return <Note title={noteItem.title} content={noteItem.content} />
+        return (
+          <Note 
+            title={noteItem.title} 
+            content={noteItem.content} 
+            onDelete={deleteNote}
+          />
+        );
       })}
       
       <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Note(props) {
+
+  function handleClick(){
+    props.onDelete();
+  }
+
   return (
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button>Delete</button>
+            <button onClick={handleClick}>Delete</button>
         </div>
   );
   
